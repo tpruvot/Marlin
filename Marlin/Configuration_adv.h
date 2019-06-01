53089d715ceaaac3f838140dc7378a9044910e1c
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2019-06-01 02:10:06 +0200

    bltouch config (china touch, see next commit for BLTouch 3.1)

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -893,6 +893,7 @@
    *
    * Settings for all BLTouch and clone probes:
    */
+  #undef PROBE_MANUALLY
 
   // Safety: The probe needs time to recognize the command.
   //         Minimum command delay (ms). Enable and increase if needed.
@@ -922,7 +923,7 @@
    * differs, a mode set eeprom write will be completed at initialization.
    * Use the option below to force an eeprom write to a V3.1 probe regardless.
    */
-  //#define BLTOUCH_SET_5V_MODE
+  #define BLTOUCH_SET_5V_MODE
 
   /**
    * Safety: Activate if connecting a probe with an unknown voltage mode.
@@ -1015,7 +1016,7 @@
 //
 // Add the G35 command to read bed corners to help adjust screws. Requires a bed probe.
 //
-//#define ASSISTED_TRAMMING
+#define ASSISTED_TRAMMING
 #if ENABLED(ASSISTED_TRAMMING)
 
   // Define positions for probe points.

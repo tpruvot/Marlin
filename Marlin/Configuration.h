abe1e81c5a8938bcd6d1961d983dea1c00f538ef
SupraDolph <supradolph@hotmail.com>
2021-05-17 20:26:53 +0200

    Add BMC Extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -864,7 +864,9 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 98 }
+// FHU Extrudeur d'origine : 98
+// FHU Extrudeur BMC       : 426.09
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 426.1 }
 
 /**
  * Default Max Feed Rate (mm/s)

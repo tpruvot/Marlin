cb973c9e9be97d0d8fa0a97ce9c322de91ee42c7
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2021-08-17 08:11:22 +0200

    bltouch: ensure E & part fan are off (config)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1358,8 +1358,8 @@
   //#define WAIT_FOR_BED_HEATER     // Wait for bed to heat back up between probes (to improve accuracy)
   //#define WAIT_FOR_HOTEND         // Wait for hotend to heat back up between probes (to improve accuracy & prevent cold extrude)
 #endif
-//#define PROBING_FANS_OFF          // Turn fans off when probing
-//#define PROBING_ESTEPPERS_OFF     // Turn all extruder steppers off when probing
+#define PROBING_FANS_OFF            // Turn fans off when probing
+#define PROBING_ESTEPPERS_OFF       // Turn all extruder steppers off when probing
 //#define PROBING_STEPPERS_OFF      // Turn all steppers off (unless needed to hold position) when probing (including extruders)
 //#define DELAY_BEFORE_PROBING 200  // (ms) To prevent vibrations from triggering piezo sensors
 

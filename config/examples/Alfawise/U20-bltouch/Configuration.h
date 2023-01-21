38c1b1e454e811fc34561eddbcd13a8109621112
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2023-01-21 17:19:54 +0100

    ci: update build test samples TFT_ROTATION

--- config/examples/Alfawise/U20-bltouch/Configuration.h
+++ config/examples/Alfawise/U20-bltouch/Configuration.h
@@ -3268,7 +3268,7 @@
  *   TFT_ROTATE_270, TFT_ROTATE_270_MIRROR_X, TFT_ROTATE_270_MIRROR_Y,
  *   TFT_MIRROR_X, TFT_MIRROR_Y, TFT_NO_ROTATION
  */
-//#define TFT_ROTATION TFT_NO_ROTATION
+#define TFT_ROTATION TFT_NO_ROTATION
 
 //=============================================================================
 //============================  Other Controllers  ============================
